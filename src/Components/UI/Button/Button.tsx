bfe499c049705e8e6cf90305124fc6da78bf317a
ayanawye <kaichybekovaayana@gmail.com>
2023-04-24 15:00:04 +0600

    download images

--- src/Components/UI/Button/Button.tsx
+++ src/Components/UI/Button/Button.tsx
@@ -1,11 +1,12 @@
 import { FC, ReactNode } from "react";
+import s from './Button.module.scss'
 
 interface IButton {
   children: ReactNode;
 }
 
-const Button: FC<IButton> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const Button: FC<IButton> = ({ children, ...props}) => {
+  return <button className={s.btn} {...props}>{children}</button>;
 };
 
 export default Button;
